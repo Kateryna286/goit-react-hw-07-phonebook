d0113d749a68cdb2eedc9303a048d0bdb3451595
Kateryna Karpova <katyakarpova@gmail.com>
2021-10-11 12:33:12 +0200

    Удален код отвечающий за хранение и чтение контактов из локального хранилища.

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,53 +1,11 @@
 import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
-// import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-// import {
-//   persistStore,
-//   persistReducer,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contacts/contacts-reducer';
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
 });
 
-// const contactsPersistConfig = {
-//   key: 'Contacts',
-//   storage,
-//   blacklist: ['filter'],
-// };
-
-// // чтоб убрать ошибку из консоли
-// const middleware = [
-//   ...getDefaultMiddleware({
-//     serializableCheck: {
-//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//     },
-//   }),
-// ];
-
-// const store = configureStore({
-//   reducer: {
-//     contacts: persistReducer(contactsPersistConfig, contactsReducer),
-//   },
-//   middleware,
-//   //devTools: process.env.NODE_ENV === 'development',
-// });
-
 const store = createStore(rootReducer, composeWithDevTools());
-
-// const persistor = persistStore(store);
-
-// const moduleDefaultForExport = { store, persistor };
-
-// export default moduleDefaultForExport;
 
 export default store;
