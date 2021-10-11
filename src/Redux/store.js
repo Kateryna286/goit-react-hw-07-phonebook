ad74c9d89557bc09babb69c6ec0ba801d0d942dd
Kateryna Karpova <katyakarpova@gmail.com>
2021-10-11 12:30:28 +0200

    Закомментирован код отвечающий за хранение и чтение контактов из локального хранилища.

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,42 +1,53 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+// import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contacts/contacts-reducer';
 
-const contactsPersistConfig = {
-  key: 'Contacts',
-  storage,
-  blacklist: ['filter'],
-};
-
-// чтоб убрать ошибку из консоли
-const middleware = [
-  ...getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
-];
-
-const store = configureStore({
-  reducer: {
-    contacts: persistReducer(contactsPersistConfig, contactsReducer),
-  },
-  middleware,
-  //devTools: process.env.NODE_ENV === 'development',
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
 });
 
-const persistor = persistStore(store);
+// const contactsPersistConfig = {
+//   key: 'Contacts',
+//   storage,
+//   blacklist: ['filter'],
+// };
 
-const moduleDefaultForExport = { store, persistor };
+// // чтоб убрать ошибку из консоли
+// const middleware = [
+//   ...getDefaultMiddleware({
+//     serializableCheck: {
+//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//     },
+//   }),
+// ];
 
-export default moduleDefaultForExport;
+// const store = configureStore({
+//   reducer: {
+//     contacts: persistReducer(contactsPersistConfig, contactsReducer),
+//   },
+//   middleware,
+//   //devTools: process.env.NODE_ENV === 'development',
+// });
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+// const persistor = persistStore(store);
+
+// const moduleDefaultForExport = { store, persistor };
+
+// export default moduleDefaultForExport;
+
+export default store;
