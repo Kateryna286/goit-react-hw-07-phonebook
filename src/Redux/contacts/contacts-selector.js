271bae40473c5deb41acbe0010a7fed2231a3e05
Kateryna Karpova <katyakarpova@gmail.com>
2021-10-14 13:16:07 +0200

    мемоизация селекторов

--- src/Redux/contacts/contacts-selector.js
+++ src/Redux/contacts/contacts-selector.js
@@ -1,14 +1,27 @@
+import { createSelector } from 'reselect';
+
 export const getItems = state => state.contacts.items;
 
 export const getFilter = state => state.contacts.filter;
 
-export const getVisibleContacts = state => {
-  const contacts = getItems(state);
-  const filter = getFilter(state);
+// export const getVisibleContactsOld = state => {
+//   const contacts = getItems(state);
+//   const filter = getFilter(state);
 
-  const normalizedFilter = filter.toLowerCase();
+//   const normalizedFilter = filter.toLowerCase();
 
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter),
-  );
-};
+//   return contacts.filter(({ name }) =>
+//     name.toLowerCase().includes(normalizedFilter),
+//   );
+// };
+
+export const getVisibleContacts = createSelector(
+  [getItems, getFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter),
+    );
+  },
+);
